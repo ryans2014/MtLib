9b89e78cc9878cbe6f004c69ecac1297e20fa494
Yiran Su <ryansu2011@gmail.com>
2019-09-30 19:21:29 -0400

    finished the thread pool class, not working yet

---
+++ mtlib/ThreadPool.cpp
@@ -0,0 +1,74 @@
+#include "ThreadPool.h"
+#include <thread>
+#include <functional>
+
+namespace MtLib {
+
+   ThreadPool *ThreadPool::single_instance = nullptr;
+
+   void ThreadPool::init(int max_num_threads = -1) {
+      if (ThreadPool::single_instance == nullptr) 
+         ThreadPool::single_instance = new ThreadPool(max_num_threads);
+   }
+
+   ThreadPool *ThreadPool::fetch() {
+      if (ThreadPool::single_instance == nullptr)
+         ThreadPool::single_instance = new ThreadPool();
+      return ThreadPool::single_instance;
+   }
+
+   ThreadPool::ThreadPool(int max_num_threads) {
+      // use hardware thread if not specified
+      if (max_num_threads <= 0)
+         max_num_threads = std::thread::hardware_concurrency();
+      // for safety purpose
+      if (max_num_threads < 1) max_num_threads = 1;
+      // create threads, std::thread is movable
+      for (int i = 0; i < max_num_threads; i++) {
+         auto thread_loop = std::bind(&ThreadPool::thread_running_loop, this);
+         thread_pool.push_back(std::thread(thread_loop));
+      }
+   }
+
+   ThreadPool::~ThreadPool() {
+      // notify all running thread_running_loop to return 
+      exit_flag = true;
+      tp_cv.notify_all();
+      // wait for all threads to join
+      for (auto &t : thread_pool)
+         t.join();
+   }
+
+   void ThreadPool::run(Task &&tsk) {
+      // insert job to the queue within the lock scope
+      {
+         std::lock_guard<std::mutex> lck(tp_mutex);
+         task_queue.push(std::move(tsk));
+      }
+      // notify one waiting thread outside the lock scope
+      tp_cv.notify_one();
+   }
+
+   void ThreadPool::thread_running_loop() {
+      Task task;
+      while (true) {
+         // get the next job in queue
+         {
+            // lock the mutex that protects the queue
+            std::unique_lock<std::mutex> lck(tp_mutex);
+            // if empty job queue, release the lock and wait for signal
+            // then, other thread may also come in and wait here for signal
+            while (task_queue.empty())
+               tp_cv.wait(lck);
+            // to exit, set exit_flag and notify_all
+            if (exit_flag) return;
+            // get the job
+            task = std::move(task_queue.front());
+            task_queue.pop();
+         }
+         // run the job outside the lock scope
+         task.run();
+      }
+   }
+}
+
---
+++ mtlib/ThreadPool.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <queue>
+
+namespace MtLib {
+   class Task;
+
+   class ThreadPool {
+
+   public:
+      /* create the global thread pool */
+      static void init(int max_num_threads = -1);
+
+      /* get unique thread pool instance */
+      static ThreadPool *fetch();
+     
+      /* run a task on the next available thread, non-blocking */
+      void run(Task &&tsk, int task_group = -1);
+
+      /* join a certain group of task, if task_group < 0 join all */
+      void join(int task_goup = -1);
+
+      /* wait for all thread to join before destroy */
+      ~ThreadPool();
+
+   private:
+
+      /* can only have 1 ThreadPool instance */
+      static ThreadPool *single_instance;
+
+      /* construct certain number of threads, if <= 0, use the hardware to determine */
+      ThreadPool(int max_num_threads = -1);
+      
+      /* condition_variable that notify the idled thread to continue working */
+      std::condition_variable tp_cv;
+
+      /* fixed length vector of threads */
+      std::vector<std::thread> thread_pool;
+
+      /* task queue and the mutex that protect it */
+      std::queue<Task> task_queue;
+      std::mutex tp_mutex;
+
+      /* running loop for slave threads, running endlessly until exit_flag is set to true */
+      void thread_running_loop();
+      bool exit_flag = false;
+
+   };
+
+}
